e56ed24a0520f418aeee6f6635d2ab9eacaab0af
jatin sharma <jatin2010sharma@gmail.com>
2022-12-15 18:52:36 +0530

    - sonar qube

--- src/main/java/com/example/Experiment/demoMyZone/Controller/DepartmentController.java
+++ src/main/java/com/example/Experiment/demoMyZone/Controller/DepartmentController.java
@@ -1,47 +1,8 @@
 package com.example.Experiment.demoMyZone.Controller;
 
-import com.example.Experiment.demoMyZone.models.Department;
-import com.example.Experiment.demoMyZone.request.DepartmentRequest;
-import com.example.Experiment.demoMyZone.request.DepartmentUpdateRequest;
-import com.example.Experiment.demoMyZone.service.DepartmentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.List;
 
-@RestController
 public class DepartmentController {
-
-    @Autowired
-    DepartmentService departmentService;
-
-
-    //1. -------------------------------------------------------------------------------------------------
-    @PostMapping("/department")
-    public String saveDepartment(@Valid @RequestBody DepartmentRequest departmentRequest){
-        return "";
-    }
-    //2. -------------------------------------------------------------------------------------------------
-    @GetMapping("/department/alldata")
-    public List<Department> getDapartmentdata(){
-        return departmentService.getDepartmentData();
-    }
-    //3. -------------------------------------------------------------------------------------------------
-    @GetMapping("/department/id")
-    public Department departmentById(@RequestParam("id") int id){
-        return  departmentService.getById(id);
-    }
-    //4. -------------------------------------------------------------------------------------------------
-    @DeleteMapping("/department/id")
-    public void deleteIdOfDepartment(@RequestParam("id") int id){
-        departmentService.departmentIddelete(id);
-    }
-    //5. -------------------------------------------------------------------------------------------------
-    @PutMapping("/department/update/{id}")
-    public Department updateDepartmentData(@PathVariable("id") int depatmentId, @RequestBody DepartmentUpdateRequest departmentUpdateRequest){
-
-        return departmentService.updateDepartment(depatmentId, departmentUpdateRequest);
-    }
+	
 
 }
